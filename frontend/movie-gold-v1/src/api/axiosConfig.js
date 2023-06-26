3f1d89f0d081653ca65ceb3685f969eacf0f9ee2
Chris-Quan <chrisquan86@outlook.com>
2023-06-26 07:34:32 -0400

    removed the need for ngrok for local testing

--- frontend/movie-gold-v1/src/api/axiosConfig.js
+++ frontend/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://263d-99-226-37-109.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    //baseURL:'https://263d-99-226-37-109.ngrok-free.app',
+    //headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: 'http://localhost:8080/'
+
+    //changing base url to make it easier to deploy locally without the need of ngrok
 });
